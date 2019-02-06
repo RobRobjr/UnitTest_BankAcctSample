2462e2c43eba7fb9d4a3f123641f218881988f2b
Robert Robinson <robinson6113@student.cptc.edu>
2019-02-06 13:50:14 -0800

    Set up UnitTest for a sample bank account, including properties and methods in the account class

---
+++ UnitTest_BankAcctSample/Account.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest_BankAcctSample
+{
+    /// <summary>
+    /// Represents a simple checking account at a bank
+    /// </summary>
+    public class Account
+    {
+        public Account(string AccountNum)
+        {
+            AccountNumber = AccountNum;
+        }
+
+        public string AccountNumber { get; private set; }
+
+        public string Name { get; set; }
+
+        public double Balance { get; private set; }
+
+        /// <summary>
+        /// Withdraws a specified amount from this bank account
+        /// </summary>
+        /// <param name="amount">The amount to withdraw</param>
+        /// <returns>Returns the new balance</returns>
+        public double Withdraw(double amount)
+        {
+            //TODO: Ensure there is money to be withdrawn
+            //TODO: Add a limit to withdraw
+            //TODO: Ensure the amount is positive
+            Balance -= amount;
+            return Balance;
+        }
+
+        /// <summary>
+        /// Deposits a specified amount from this bank account
+        /// </summary>
+        /// <param name="amount">The amount being deposited</param>
+        /// <returns>Current balance</returns>
+        public double Deposit(double amount)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ UnitTest_BankAcctSampleTests/AccountTests.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UnitTest_BankAcctSample;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest_BankAcctSample.Tests
+{
+    [TestClass]//Parenthesis are optional
+    public class AccountTests
+    {
+        [TestMethod()]
+        public void WithdrawTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
